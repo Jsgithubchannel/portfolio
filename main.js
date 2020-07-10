800cb5b8657cd3d4b4c0e55902509fb98e34e238
JS <jsschannel99@gmail.com>
2020-07-11 01:40:13 +0900

    JS: Navbar menu toggle + project UI tweak

--- main.js
+++ main.js
@@ -17,7 +17,14 @@ navbarMenu.addEventListener("click", (event) => {
   const target = event.target;
   const link = target.dataset.link;
   if (link == null) return;
+  navbarMenu.classList.remove("open");
   scrollIntoView(link);
+});
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector(".navbar__toggle-btn");
+navbarToggleBtn.addEventListener("click", () => {
+  navbarMenu.classList.toggle("open");
 });
 
 // Handle click on "Contact Me" button on home
